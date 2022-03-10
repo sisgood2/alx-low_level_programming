9a67a255b7e8f4d3ce2cd48f8e628260a08c29b5
sisgood <sisaymoges55@gmail.com>
2022-03-10 12:55:54 -0800

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Descrption: Main - print out the standard output
+ * Return: o if success
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+retur (0);
+}
